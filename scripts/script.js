fed8dcf0961b61fe5583bb0de5d9ce2f315fb24f
rizayldrm <rizayildirim126@gmail.com>
2023-10-30 16:52:29 +0300

    #

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
 const closenav = document.getElementById('closeNav')
 const opennav = document.getElementById('openNav')
 const linksmenu = document.getElementById('linksmenu')
+const vanishsound = new Audio('./audio/vanish.mp3')
+const contactl1 = document.getElementById('contactl1')
+const contactl2 = document.getElementById('contactl2')
+const contactl3 = document.getElementById('contactl3')
+const contactl4 = document.getElementById('contactl4')
+const contactl5 = document.getElementById('contactl5')
+const formsubmitbtn = document.getElementById('formsubmitbtn')
 
 opennav.addEventListener('click',()=>{
     linksmenu.classList.add('activenav')
@@ -9,3 +16,25 @@ opennav.addEventListener('click',()=>{
 closenav.addEventListener('click',()=>{
     linksmenu.classList.remove('activenav')
 })
+
+function contactl1vanish() {
+    vanishsound.play()
+    contactl1.style.display = 'none'
+}
+function contactl2vanish() {
+    vanishsound.play()
+    contactl2.style.display = 'none'
+}
+function contactl3vanish() {
+    vanishsound.play()
+    contactl3.style.display = 'none'
+}
+function opendclink() {
+    window.open('https://discord.com/users/718798893445283863')
+}
+function opengitlink() {
+    window.open('https://github.com/rizayldrm')
+}
+function icantbackend() {
+    formsubmitbtn.innerHTML = 'Hala veritabanı kuramıyom 😭'
+}
